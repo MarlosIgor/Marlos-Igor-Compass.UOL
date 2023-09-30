88a662a99f4f2c15fd0051e5237dedbb22bfde1b
MarlosIgor <maroosigor@gmail.com>
2023-09-30 12:53:58 -0300

    update df-01

--- Sprint 7/exercicios/seção-5-desafio-parte-I/desafio-parte-1-etl.py
+++ Sprint 7/exercicios/seção-5-desafio-parte-I/desafio-parte-1-etl.py
@@ -1,16 +1,17 @@
-import boto3
 import os
+import boto3
 from datetime import datetime
 
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAVMELNHFFVNEVPOEJ'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 
+s3 = boto3.client('s3',
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key)
 
 source_directory = "/data"
-s3_bucket = "marlos-igor"
+s3_bucket = "data-lake-marlos-igor"
 s3_prefix = "Raw/Local/CSV"
-
-s3 = boto3.client("s3")
 
 files_to_upload = ["movies.csv", "series.csv"]
 
@@ -22,6 +23,4 @@ for file_name in files_to_upload:
     s3.upload_file(os.path.join(source_directory,
                    file_name), s3_bucket, s3_key)
     print(f"Arquivo {file_name} carregado com sucesso para o S3.")
-
-
 
